ec1fb7f55a6744b402d660eb292de8dd51748db0
zhaohaiyang <413217851@qq.com>
2016-02-08 10:42:46 +0800

    提交资源文件

--- app/src/main/java/com/zhy/springfestival/MainActivity.java
+++ app/src/main/java/com/zhy/springfestival/MainActivity.java
@@ -2,8 +2,10 @@ package com.zhy.springfestival;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.Window;
@@ -11,7 +13,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends Activity implements View.OnClickListener {
 
     private ImageView photoView;
     private EditText contentView;
@@ -24,16 +31,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         initView();
 
-        photoView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK, null);
-                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-                startActivityForResult(intent, 1000);
-            }
-        });
 
-        contentView.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/test.ttf"));
+        contentView.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/test.ttf"));
+        shareButton.setOnClickListener(this);
+        photoView.setOnClickListener(this);
     }
 
     private void initView() {
@@ -48,5 +49,49 @@ public class MainActivity extends Activity {
         if (resultCode == RESULT_OK && requestCode == 1000) {
             photoView.setImageURI(data.getData());
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_photo:
+                Intent intent = new Intent(Intent.ACTION_PICK, null);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, 1000);
+                break;
+            case R.id.btn_share:
+                createPic();
+                break;
+        }
+    }
+
+    private void createPic() {
+        shareButton.setVisibility(View.GONE);
+        contentView.clearFocus();
+        contentView.setFocusable(false);
+        View view = getWindow().getDecorView();
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap bitmap = view.getDrawingCache();
+        String path = Environment.getExternalStorageDirectory().getPath() + "/springfastival.png";
+        saveBitmap(bitmap, path);
+    }
+
+    public void saveBitmap(Bitmap bm, String picName) {
+        File f = new File(picName);
+        if (f.exists()) {
+            f.delete();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(f);
+            bm.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
